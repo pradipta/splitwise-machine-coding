16f1dba5c5b819ff980e0a5f0fd51047f09951b4
Pradipta Sarma <pradiptasarma@outlook.com>
2019-10-30 12:08:22 +0530

    split type cases

--- src/SplitwiseService.java
+++ src/SplitwiseService.java
@@ -4,17 +4,16 @@ import java.util.List;
 public class SplitwiseService {
 	public static Expense createExpense(ExpenseType type, double amount, User payee, List<Split> splits) {
 		switch (type) {
-		case EQUAL:
+		//change to cases for EQUAL, EXACT, PERCENT when implemented
+		default:
 			int totalSplits = splits.size();
 			double splitAmount = ((double) Math.round(amount * 100 / totalSplits)) / 100;
 			for (Split split : splits) {
 				split.setAmount(splitAmount);
 			}
 			//to avoid decimal error. Like 100/3 = 33.33, adds up to 99.99. To avoid that
-			splits.get(0).setAmount(splutAmount + amount - splitAmount*totalSplits);
+			splits.get(0).setAmount(splitAmount + amount - splitAmount*totalSplits);
 			return new EqualExpense(amount, payee, new Date(), splits);
-		default:
-			System.out.println("Not implemented yet");
 		}
 	}
 }
