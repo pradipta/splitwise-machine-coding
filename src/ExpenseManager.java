71d8dd0a1ef9be7b6adb9f6e362b86500b89449c
Pradipta Sarma <pradiptasarma@outlook.com>
2019-10-30 12:33:52 +0530

    expense manager

--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class ExpenseManager {
 	List<Expense> expenses;
-	//userMap: userId to User
+	// userMap: userId to User
 	Map<String, User> userMap;
 	Map<String, Map<String, Double>> balanceSheet;
 
@@ -11,9 +11,15 @@ public class ExpenseManager {
 		this.userMap = new HashMap<String, User>();
 		this.balanceSheet = new HashMap<String, Map<String, Double>>();
 	}
-	
+
 	public void addUder(User user) {
 		userMap.put(user.getId(), user);
 		balanceSheet.put(user.getId(), new HashMap<String, Double>());
+	}
+	
+	public void addExpense (ExpenseType type, double amount, User payee, List<Split> splits) {
+		//TODO add expense, add to expenses list, add to balance sheet
+		Expense newExpense = SplitwiseService.createExpense(type, amount, payee, splits);
+		expenses.add(newExpense);
 	}
 }
