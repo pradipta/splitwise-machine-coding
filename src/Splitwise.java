d6003da8c99e2a76d059993177a0225f30bf3cae
Pradipta Sarma <pradiptasarma@outlook.com>
2019-10-31 08:51:19 +0530

    user input create equal

--- src/Splitwise.java
+++ src/Splitwise.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-//import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,23 +11,6 @@ public class Splitwise {
 		expenseManager.addUser(new User("hemant", "Hemant Saikia", "hmnt.ska23@gmail.com", "7123123123"));
 		expenseManager.addUser(new User("asmita", "Asmita Dutta", "asmita.dutta@outlook.com", "8133910729"));
 		expenseManager.addUser(new User("jishnu", "Jishnu Sankar Bora", "jb@al.com", "1231231232"));
-//
-//		String paidBy = "pradipta";
-//		Double amount = (double) 100;
-//		//int noOfUsers = 4;
-//		List<Split> splits = new ArrayList<>();
-//		String label = "IceCream";
-//		//Make splits
-//
-//		splits.add(new EqualSplit(expenseManager.userMap.get("pradipta"), amount));
-//		splits.add(new EqualSplit(expenseManager.userMap.get("nasir"), amount));
-//		splits.add(new EqualSplit(expenseManager.userMap.get("asmita"), amount));
-//		splits.add(new EqualSplit(expenseManager.userMap.get("hemant"), amount));
-//		splits.add(new EqualSplit(expenseManager.userMap.get("jishnu"), amount));
-//
-//		expenseManager.addExpense(ExpenseType.EQUAL, amount, paidBy, splits, label);
-//
-//		expenseManager.showBalanceForUser("pradipta");
 
 		Scanner scanner = new Scanner(System.in);
 		while (true){
@@ -40,14 +22,17 @@ public class Splitwise {
 					createExpense(expenseManager, scanner);
 					break;
 				case "2":
-					viewBalance();
+					viewBalance(expenseManager, scanner);
 					break;
 			}
 		}
 	}
 
-	private static void viewBalance() {
-		//TODO
+	private static void viewBalance(ExpenseManager expenseManager, Scanner scanner) {
+		System.out.println("Enter userid for whom you want to see balance:");
+		String userId = scanner.nextLine();
+		//TODO put check
+		expenseManager.showBalanceForUser(userId);
 	}
 
 	private static void createExpense(ExpenseManager expenseManager, Scanner scanner) {
@@ -78,6 +63,6 @@ public class Splitwise {
 			System.out.println("Invalid type. Only equal supported for now.");
 		}
 
-		expenseManager.showBalanceForUser("pradipta");
+		expenseManager.showBalanceForUser(paidBy);
 	}
 }
