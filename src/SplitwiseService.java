32f8e7780bd2dc1d625aa33773a08e793e1bef52
Pradipta Sarma <pradiptasarma@outlook.com>
2019-10-31 14:44:33 +0530

    percent expense

--- src/SplitwiseService.java
+++ src/SplitwiseService.java
@@ -5,7 +5,7 @@ public class SplitwiseService {
 	public static Expense createExpense(ExpenseType type, double amount, User paidBy, List<Split> splits, String label) {
 		switch (type) {
 		//change to cases for EQUAL, EXACT, PERCENT when implemented
-		default:
+		case EQUAL:
 			int totalSplits = splits.size();
 			double splitAmount = ((double) Math.round(amount * 100 / totalSplits)) / 100;
 			for (Split split : splits) {
@@ -14,6 +14,19 @@ public class SplitwiseService {
 			//to avoid decimal error. Like 100/3 = 33.33, adds up to 99.99. To avoid that
 			splits.get(0).setAmount(splitAmount + amount - splitAmount*totalSplits);
 			return new EqualExpense(amount, paidBy, new Date(), splits, label);
+
+		case PERCENT:
+			for (Split split:splits){
+				PercentSplit percentSplit = (PercentSplit) split;
+				//double percentAmount = Math.round(amount*percentSplit.getPercent()/100);
+				split.setAmount(amount*percentSplit.getPercent()/100);
+			}
+			return new PercentExpense(amount, paidBy, new Date(), splits, label);
+		case EXACT:
+
+		default:
+			System.out.println("Invalid split type");
 		}
+
 	}
 }
